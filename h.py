3068a8c34fcbbda0782ee1819c1d943f08fdbe9f
rayan20-meet <rayan20@meet.mit.edu>
2018-08-05 15:28:03 -0400

    Stopping here

---
+++ h.py
@@ -0,0 +1,16 @@
+indentation = True
+if indentation:
+    print('choclate')
+print('Indentations are cool!')    
+num1 = 2
+num2 = 7
+num3 = 5
+
+if num1 > num2:
+    print('turtles')
+elif num1 > num3:
+    print('cats')
+elif num1 == num3:
+    print('fish')
+else:
+    print('dogs')
